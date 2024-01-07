6b061e989423aac0629db4a5b8981609d73dd5c9
Lou Knauer <lou.knauer@sipearl.com>
2024-01-07 08:02:15 +0100

    some subset of typechecking works

--- 025-rhall/examples/fibs.dhall
+++ 025-rhall/examples/fibs.dhall
@@ -1,1 +1,1 @@
-let fib = λ(n: Int) -> if n < 2 then 1 else fib(n - 1) + fib(n - 2) in fib(10)
+let fib: ∀(n: Int) -> Int = λ(n: Int) -> if n < 2 then 1 else fib(n - 1) + fib(n - 2) in fib(10)
--- 025-rhall/src/core.rs
+++ 025-rhall/src/core.rs
@@ -25,19 +25,18 @@ pub enum Error {
     UndefinedValue(Rc<str>),
     Uncallable(SLoc, String),
     ExpectedType(SLoc),
-    TypeError(SLoc, String)
+    TypeError(SLoc, String),
 }
 
 // TODO: Do something string_pool like for types?
 // As types will basically never change but be shared/cross-reference a lot
 // that would be nice and beneficial.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub enum Type {
     Boolean,
     Integer,
     String,
-    Type,
-    TypeType,
+    Type(Option<Rc<Type>>),
     Lambda(Vec<(Rc<str>, Rc<Type>)>, Rc<Type>),
 }
 
@@ -47,8 +46,8 @@ impl Display for Type {
             Type::Boolean => write!(f, "Bool"),
             Type::Integer => write!(f, "Int"),
             Type::String => write!(f, "Str"),
-            Type::Type => write!(f, "Type"),
-            Type::TypeType => write!(f, "TypeType"),
+            Type::Type(None) => write!(f, "Type"),
+            Type::Type(Some(t)) => write!(f, "Type /* {} */", t.as_ref()),
             Type::Lambda(args, rettyp) => {
                 write!(f, "∀(")?;
                 for (i, (name, argtyp)) in args.iter().enumerate() {
@@ -60,23 +59,42 @@ impl Display for Type {
     }
 }
 
+impl PartialEq for Type {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Type::Boolean, Type::Boolean) => true,
+            (Type::Integer, Type::Integer) => true,
+            (Type::String, Type::String) => true,
+            (Type::Type(Some(t1)), Type::Type(Some(t2))) => t1 == t2,
+            (Type::Type(_), Type::Type(_)) => true, // TODO: Check stuff like: 1 -> Int -> Type -> Kind...
+            (Type::Lambda(args1, rettyp1), Type::Lambda(args2, rettyp2)) =>
+                args1.len() == args2.len() &&
+                args1.iter().zip(args2.iter()).all(|((_, t1), (_, t2))| t1.as_ref() == t2.as_ref()) &&
+                rettyp1.as_ref() == rettyp2.as_ref(),
+            (_, _) => false
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Value {
+    Pseudo(Rc<Type>),
     Bool(bool),
     Int(i64),
     Str(Rc<str>),
-    Type(Option<Rc<str>>, Rc<Type>),
+    Type(Rc<Type>),
     Lambda(Vec<(Rc<str>, Rc<Type>)>, Rc<RefCell<Node>>),
 }
 
 impl Value {
     pub fn get_type(&self, env: &Env) -> Rc<Type> {
         match self {
+            Value::Pseudo(t) => t.clone(),
             Value::Bool(_) => env.bool_type.clone(),
             Value::Int(_) => env.int_type.clone(),
             Value::Str(_) => env.str_type.clone(),
-            Value::Type(_, _) => env.type_type.clone(),
-            Value::Lambda(args, body) => Rc::new(Type::Lambda(args.clone(), body.borrow().get_type())),
+            Value::Type(t) => Rc::new(Type::Type(Some(t.clone()))),
+            Value::Lambda(args, body) => Rc::new(Type::Lambda(args.clone(), body.borrow().get_type().unwrap())),
         }
     }
 }
@@ -84,12 +102,12 @@ impl Value {
 impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Value::Pseudo(t) => write!(f, "(PSEUDO:{})", t.as_ref()),
             Value::Bool(true) => write!(f, "true"),
             Value::Bool(false) => write!(f, "false"),
             Value::Int(x) => write!(f, "{}", x),
             Value::Str(s) => write!(f, "{:?}", s.as_ref()),
-            Value::Type(None, t) => Display::fmt(t.as_ref(), f),
-            Value::Type(Some(name), t) => write!(f, "{} /* supertype: {} */", name.as_ref(), t.as_ref()),
+            Value::Type(t) => Display::fmt(t.as_ref(), f),
             Value::Lambda(args, node) => {
                 write!(f, "λ(")?;
                 for (i, (name, typ)) in args.iter().enumerate() {
